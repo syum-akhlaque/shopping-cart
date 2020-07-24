2da64d79d1e568cba402b8e4e9437ff5d7152395
syum-akhlaque <arssyum@gmail.com>
2020-07-24 16:36:21 +0600

    plus minus icon function complete

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 //remove item hendeler
-document.getElementById("removeItem").addEventListener("click",function(){
-    var itemValue = updateRemoveItemValue("itemValue")
+
+document.getElementById("removeItem1").addEventListener("click",function(){
+    var itemValue = updateRemoveItemValue("item1Value");
+})
+document.getElementById("removeItem2").addEventListener("click",function(){
+    var itemValue = updateRemoveItemValue("item2Value");
 })
 //add item hendeler
-document.getElementById("addItem").addEventListener("click",function(){
-    var itemValue = updateAddItemValue("itemValue") 
+document.getElementById("addItem1").addEventListener("click",function(){
+    var itemValue = updateAddItemValue("item1Value") ;
+    
+})
+document.getElementById("addItem2").addEventListener("click",function(){
+    var itemValue = updateAddItemValue("item2Value") ;
+   
 })
 
 
 function updateRemoveItemValue(id){
     var itemValue = document.getElementById(id).value;
+    itemValue = parseInt(itemValue);
     if(itemValue>=2){
         itemValue=itemValue-1;
     }
-   document.getElementById("itemValue").value=itemValue;
+   document.getElementById(id).value=itemValue;
 }
 function updateAddItemValue(id){
-    var itemValue = document.getElementById("itemValue").value;
+    var itemValue = document.getElementById(id).value;
     itemValue = parseInt(itemValue);
     itemValue = itemValue+1;  
-    console.log(itemValue);
-    document.getElementById("itemValue").value=itemValue;
+    document.getElementById(id).value=itemValue;
 }
